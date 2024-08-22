dda8d41612c7970fe081a67fbfd05878af7c97c1
Joel Wurtz <jwurtz@jolicode.com>
2024-08-22 10:12:01 +0200

    fix(schema): support unkown type for collection as object (string key)

--- src/JsonSchema/SchemaFactory.php
+++ src/JsonSchema/SchemaFactory.php
@@ -179,7 +179,8 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
         $propertySchemaType = $propertySchema['type'] ?? false;
 
         $isUnknown = Schema::UNKNOWN_TYPE === $propertySchemaType
-            || ('array' === $propertySchemaType && Schema::UNKNOWN_TYPE === ($propertySchema['items']['type'] ?? null));
+            || ('array' === $propertySchemaType && Schema::UNKNOWN_TYPE === ($propertySchema['items']['type'] ?? null))
+        || ('object' === $propertySchemaType && Schema::UNKNOWN_TYPE === ($propertySchema['additionalProperties']['type'] ?? null));
 
         if (
             !$isUnknown && (
@@ -223,9 +224,17 @@ final class SchemaFactory implements SchemaFactoryInterface, SchemaFactoryAwareI
             }
 
             if ($isCollection) {
-                $propertySchema['items']['$ref'] = $subSchema['$ref'];
-                unset($propertySchema['items']['type']);
-                break;
+                $keyTYpe = $type->getCollectionKeyTypes()[0] ?? null;
+
+                if ($keyTYpe && $keyTYpe->getBuiltinType() === Type::BUILTIN_TYPE_STRING) {
+                    $propertySchema['additionalProperties']['$ref'] = $subSchema['$ref'];
+                    unset($propertySchema['additionalProperties']['type']);
+                    break;
+                } else {
+                    $propertySchema['items']['$ref'] = $subSchema['$ref'];
+                    unset($propertySchema['items']['type']);
+                    break;
+                }
             }
 
             $refs[] = ['$ref' => $subSchema['$ref']];
